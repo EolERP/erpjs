e6ea67555f6b2bba38dddbe543e531a8af4bddcc
dpodhola-eolerp <david.podhola@eolerp.eu>
2024-09-07 06:56:33 +0200

    Delete clients directory as they will have their own repo

--- clients/admin/src/absorb/svelte-forms/src/rules/between.ts
+++
@@ -1,6 +0,0 @@
-import { max } from './max';
-import { min } from './min';
-
-export function between(val: any, args: any) {
-    return min(val, [args[0]]) && max(val, [args[1]]);
-}
--- clients/admin/src/lib/core/menu.ts
+++
@@ -1,6 +0,0 @@
-import { writable } from 'svelte/store';
-import type { MenuListPartsFragment } from '../../generated/graphql';
-import { store } from '../support/store';
-
-export const menuStore = store<MenuListPartsFragment | undefined>(undefined); // start with no menu
-export const profileMenuOpened = writable<boolean>(false);
